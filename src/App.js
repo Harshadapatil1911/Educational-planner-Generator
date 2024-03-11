e3ce8e1e137ed1264500c4c0f96e445e2cf2262b
“Harshada_Patil” <patilharshada1911@gmail.com>
2024-03-11 19:20:58 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState} from 'react';
+import './CourseCard.js'
+import './planner.js'
+import Planner from './planner.js';
+
+function App() {
+    
+  
+  return (
+    <>
+    <Planner/>
+   </>
+
+  )
+  }
+
+
+
+export default App;
---
+++ src/planner.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import CourseCard from "./CourseCard";
+import styles from "./App.css"
+
+function Planner() {
+  const [course, setcourse] = useState([]);
+
+
+  const onclickHandler = () => {
+    let courseName = document.getElementById("CourseName").value;
+    let courseHours = document.getElementById("CourseHours").value;
+    
+
+   
+
+    let newData = {
+      name: courseName,
+      hours: parseInt(courseHours),
+    };
+
+    const data = JSON.parse(JSON.stringify(course));
+    
+    data.push(newData);
+    console.log(newData);
+    localStorage.setItem("data",newData)
+    if(!courseName=="" && !courseHours==""){
+      setcourse(data);
+      document.getElementById("CourseName").value=""
+      document.getElementById("CourseHours").value=""
+    }else{
+      alert("add a valid data")
+    }
+  
+  };
+  // console.log(course);
+
+  return (
+    <div>
+      <div className="main_card">
+        <h3>Geekster Educational Plannner</h3>
+        <div className="fields">
+          <input type="text" placeholder="subject" id="CourseName" />
+          <input type="number" placeholder="Hours" id="CourseHours" />
+          <button onClick={onclickHandler}>Add Data</button>
+        </div>
+
+        {course.map((course, index) => (
+          <CourseCard name={course.name} hour={course.hours} key={index} />
+        ))}
+        {/* <CourseCard/> */}
+      </div>
+    </div>
+  );
+}
+
+export default Planner;
